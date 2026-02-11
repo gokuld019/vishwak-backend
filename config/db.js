49b9c98b6e00f803104ae39ea547f8619997fcce
Mindcarve007 <dev@mindcarve.in>
2026-02-11 18:24:23 +0530

    fix railway mysql connection

--- config/db.js
+++ config/db.js
@@ -7,8 +7,23 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // IMPORTANT
     dialect: "mysql",
     logging: false,
+
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
 );
 
