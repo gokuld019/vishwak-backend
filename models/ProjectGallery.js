3043ad67b200c2a07c649184bd36c96677f4d44f
Mindcarve007 <dev@mindcarve.in>
2026-02-25 11:49:04 +0530

    production

--- models/ProjectGallery.js
+++ models/ProjectGallery.js
@@ -1,18 +1,26 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const ProjectGallery = sequelize.define("ProjectGallery", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+const ProjectGallery = sequelize.define(
+  "ProjectGallery",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 
-  projectId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
+    projectId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
 
-  image: {
-    type: DataTypes.STRING,
-    allowNull: false,
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-});
+  {
+    tableName: "projectgalleries",   // ✅ IMPORTANT
+    freezeTableName: true,           // ✅ VERY IMPORTANT
+    timestamps: true,
+  }
+);
 
 module.exports = ProjectGallery;
